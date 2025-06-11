177f4384fadcbe7a1e02b39e647b60e1a49199e0
Luiz Velasco <luizvelasco@gmail.com>
2025-06-11 17:01:24 -0300

    templates

--- index.php
+++ index.php
@@ -1,75 +1,11 @@
 <?php
-    require_once("globals.php");
-    require_once("db.php");
+    require_once("templates/header.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Personal</title>
-    <link rel="short icon" href="<?= $BASE_URL ?>img/moviestar.ico">
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.css" integrity="sha512-drnvWxqfgcU6sLzAJttJv7LKdjWn0nxWCSbEAtxJ/YYaZMyoNLovG7lPqZRdhgL1gAUfa+V7tbin8y+2llC1cw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Font Awesome -->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- CSS do projeto -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
-</head>
-<body>
-    <header>
-        <nav id="main-navbar" class="navbar navbar-expand-lg">
-            <a href="<?= $BASE_URL ?>" class="navbar-brand">
-                <img src="<?= $BASE_URL ?>img/logo.svg" alt="personal" id="logo">
-                <span id="personal-title">Personal</span>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-            </button>
-            <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-                <input type="text" name="q" id="search" class="form-control my-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
-                <button class="btn my-2 my-sm-0" type="submit">
-                    <i class="fas fa-search"></i>
- 
-                </button>
-            </form>
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar/Cadastrar</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-    <div id="main-container" class="container-fluid">
-        <h1>Corpo do Site</h1>
-    </div>
-    <footer id="footer">
-        <div id="social-container">
-            <ul>
-                <li>
-                    <a href="#"><i class="fab fa-facebook-square"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fab fa-instagram-square"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fab fa-youtube-square"></i></a>
-                </li>
-            </ul>
-        </div>
-        <div id="footer-links-container">
-            <ul>
-                <li><a href="#">Adicionar Filme</a></li>
-                <li><a href="#">Adicionar Critica</a></li>
-                <li><a href="#">Entrar / Registrar</a></li>
-            </ul>
-        </div>
-        <p>&copy; 2025 Luiz Velasco</p>
-    </footer>
-    <!-- Bootstrap JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.js" integrity="sha512-KCgUnRzizZDFYoNEYmnqlo0PRE6rQkek9dE/oyIiCExStQ72O7GwIFfmPdkzk4OvZ/sbHKSLVeR4Gl3s7s679g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+
+<div id="main-container" class="container-fluid">
+    <h1>Corpo do Site</h1>
+</div>
+    
+<?php
+    require_once("templates/footer.php");
+?>
