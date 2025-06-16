b7755e7a4870f6056a4d03e69834f3d4bf6f1965
Luiz Velasco <luizvelasco@gmail.com>
2025-06-16 16:10:41 -0300

    Registro de professor

--- auth_process.php
+++ auth_process.php
@@ -10,6 +10,8 @@
 
     $professorDao = new ProfressorDAO($conn, $BASE_URL);
 
+    $professor = new Professor();
+
     // Resgata o tipo do formulário
     $type = filter_input(INPUT_POST, "type");
 
@@ -43,7 +45,7 @@
 
                     $auth = true;
 
-                    $professorDao->create($user,$auth);
+                    $professorDao->create($professor, $auth);
 
                 } else {
                     // Enviar msg de erro, usuáriuo já existe
--- models/professor.php
+++ models/professor.php
@@ -25,5 +25,6 @@ interface ProfessorDAOInterface {
     public function buildProfessor($data);
     public function create(Professor $professor, $authProfessor = false);
     public function findByEmail($email);
+    public function setTokenToSession($token, $redirect = true);
 
 }
